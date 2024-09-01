9861d621166c03c75d55cd329fda9cf034229890
amaan <amaan@amaans-Laptop.local>
2024-09-01 15:23:05 -0700

    added new note button to UI

--- notes/app/static/app/index.js
+++ notes/app/static/app/index.js
@@ -1,49 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const notes_tab = document.getElementById('notes-tab');
-    const folders_tab = document.getElementById('folders-tab');
+    const audio_tab = document.getElementById('audio-tab');
     const notes_content = document.getElementById('notes-content');
-    const folders_content = document.getElementById('folders-content');
+    const audio_content = document.getElementById('audio-content');
 
     // toggle
     notes_tab.addEventListener('click', () => {
         notes_tab.classList.remove('btn-secondary');
         notes_tab.classList.add('btn-success');
 
-        if (folders_tab.classList.contains('btn-success')) {
-            folders_tab.classList.remove('btn-success');
-            folders_tab.classList.add('btn-secondary');
+        if (audio_tab.classList.contains('btn-success')) {
+            audio_tab.classList.remove('btn-success');
+            audio_tab.classList.add('btn-secondary');
         }
 
         notes_content.classList.remove('hidden');
-        folders_content.classList.add('hidden');
+        audio_content.classList.add('hidden');
 
         notes_tab.classList.add('active');
-        folders_tab.classList.remove('active');
+        audio_tab.classList.remove('active');
 
         notes_content.classList.remove('hidden');
-        folders_tab.classList.add('hidden');
+        audio_tab.classList.add('hidden');
     })
     
     // toggle
-    folders_tab.addEventListener('click', () => {
-        folders_tab.classList.remove('btn-secondary');
-        folders_tab.classList.add('btn-success');
+    audio_tab.addEventListener('click', () => {
+        audio_tab.classList.remove('btn-secondary');
+        audio_tab.classList.add('btn-success');
 
         if (notes_tab.classList.contains('btn-success')) {
             notes_tab.classList.remove('btn-success');
             notes_tab.classList.add('btn-secondary');
         }
 
-        folders_content.classList.remove('hidden');
+        audio_content.classList.remove('hidden');
         notes_content.classList.add('hidden');
 
-        folders_tab.classList.add('active');
+        audio_tab.classList.add('active');
         notes_tab.classList.remove('active');
 
-        folders_content.classList.remove('hidden');
+        audio_content.classList.remove('hidden');
         notes_content.classList.add('hidden');
     })
+
+    document.querySelector('#new_note').onclick = () => {
+        window.location.href = 'createdit_note/0/index';
+    }
 
     // default view
     notes_tab.click();
